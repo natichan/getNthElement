c26f7fa0d7634213e6db7ac50b7fd465c89eed19
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-06-12 23:35:44 -0400

    Ejercicio array js

---
+++ app.js
@@ -0,0 +1,9 @@
+
+function getNthElement(arreglo, numeroIndicador) {
+    return arreglo[numeroIndicador];
+    /* VOy a retornar un arreglo al que le puedo dar los elementos que elija, como los números. 
+    El número indicador es el tal como su nombre, es el indice, el número entero a seleccionar dentro del array*/
+  }
+  
+ console.log (getNthElement);
+  
